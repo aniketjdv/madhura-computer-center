ef241e8ccd1f4795f0704c28bb7f26fcd0e6e17c
Aniket <aniketjadhav310@gmail.com>
2024-08-24 13:13:54 +0530

    done some database work

--- MadhuracomputerCenter/MadhuracomputerCenter/urls.py
+++ MadhuracomputerCenter/MadhuracomputerCenter/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
@@ -25,4 +26,4 @@ urlpatterns = [
     path('contact/', views.contnct, name='contact'),
     path('klic_course/', views.klic_course, name='klic_course'),
     path('mscit/', views.mscit, name='mscit'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
