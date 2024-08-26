0748590d781a51e0107fc12db5d6999dbd0a1107
Aniket <aniketjadhav310@gmail.com>
2024-08-26 13:15:43 +0530

    files shifted to app

--- MadhuracomputerCenter/MadhuracomputerCenter/views.py
+++ MadhuracomputerCenter/MadhuracomputerCenter/views.py
@@ -5,16 +5,16 @@ def home(request):
     return render(request, 'website/index.html')
 
 def about(request):
-    return render(request, 'website/about.html')
+    return render(request, 'madhuracomputer/about.html')
 
 def contnct(request):
-    return render(request, 'website/contact.html')
+    return render(request, 'madhuracomputer/contact.html')
 
 def klic_course(request):
-    return render(request, 'website/klic-course.html')
+    return render(request, 'madhuracomputer/klic-course.html')
 
 def mscit(request):
-    return render(request, 'website/mscit.html')
+    return render(request, 'madhuracomputer/mscit.html')
 
 # def courses(request):
 #     return render(request, 'website/courses.html')
