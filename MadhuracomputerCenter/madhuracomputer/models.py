4195a377a43a50fcd36a961ec32691d76387a2ed
Aniket <aniketjadhav310@gmail.com>
2024-08-28 14:03:27 +0530

    added more data in databse

--- MadhuracomputerCenter/madhuracomputer/models.py
+++ MadhuracomputerCenter/madhuracomputer/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from django.utils import timezone
 class Klic_Course(models.Model):
     COURSE_TYPE_CHOICES=[
-('AC',"Accounting Careers"),('BOC',"Back Office Careers"),('DC',"Designing Careers"),('DAC',"Digital Arts Careers"),('DF',"Digital Freelancing"),('FOC',"Front Office Careers"),('IHANC',"IT Hardware and Networking Careers"),('MNCJ',"Management New Collar Jobs"),('PC',"Programming Careers")
+('AC',"Accounting Careers"),('BOC',"Back Office Careers"),('DC',"Designing Careers"),('DAC',"Digital Arts Careers"),('DF',"Digital Freelancing"),('FOC',"Front Office Careers"),('IHANC',"IT Hardware and Networking Careers"),('M',"Management"),('NCJ',"New Collar Jobs"),('PC',"Programming Careers")
 ]
     name=models.CharField(max_length=100)
     image=models.ImageField(upload_to="kliccourse")
